b4d75a737dc103ce60ece2e51984fdaab70f48d8
indiedot <indiedot@naver.com>
2014-02-09 06:45:00 +0900

    플래시 외부링크 테스트

---
+++ dcgall_link.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+function link(args){
+var url="";
+switch(args){
+case "1":url="http://gall.dcinside.com/board/view/?id=kwbin&no=4793","_blank";break;
+case "2":url="http://gall.dcinside.com/board/view/?id=kwbin&no=4793";break;
+case "3":url="#";break;
+case "4":url="#";break;
+case "5":url="#";break;
+case "6":url="#";break;
+case "7":url="#";break;
+case "8":url="#";break;
+}
+top.document.location.href=url
+}
